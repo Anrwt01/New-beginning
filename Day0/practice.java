423d1d31fb083b26ed1ed266f028d3fcb718c0b3
Anrwt01 <1anujrawat@gmail.com>
2024-09-24 11:36:20 +0530

    message

--- Day0/practice.java
+++ Day0/practice.java
@@ -38,16 +38,46 @@ public class practice {
             System.out.println();
     } */
 
-    for ( int i=n; i>=1; i--){
+   /*  for ( int i=n; i>=1; i--){
         for (int j=1; j<=i; j++){
                System.out.print ( "*");
            }
-           System.out.println();
+           System.out.println();*/
 
+           //printing a rhombus
+
+           /* for (int i=1; i<=n; i++){
+            for (int k=1; k<=n-i; k++){
+                System.out.print(" ");
+            }
+            for (int j=1; j<=n; j++){
+                System.out.print("*");
+            }
+            System.out.println( );
+           }
+ */
+        // for printing plaindromic 
+
+        for (int i=1; i<=n; i++){
+            for (int k=1; k<=2*n-i; k++){
+                System.out.print(" ");
+            }
+            for (int j=1; j<2*i; j++){
+                System.out.print("*");
+
+            }System.out.println( );
+
+        
+
+        }
+
+       
+       
+
+ 
 }
     
-}}
-    
+}    
     
     
     
